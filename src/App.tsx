4f5a840532372cdaecc910ec3e33efafe99030e5
PauloVitor <paulovitor.informatca@gmail.com>
2023-11-01 19:33:27 -0300

    projeto idade

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+import './App.css'
+
+interface ResultadoProps{
+  idade: number;
+  name: string;
+}
+
+function App() {
+  const [name, setName] = useState("")
+  const [idade, setIdade] = useState()
+  const [result, setResult] = useState<ResultadoProps>()
+
+  function handleResult(e: FormEvent){
+    e.preventDefault()
+
+    const currentYear = new Date().getUTCFullYear();
+    setResult({
+      name: name,
+      idade: currentYear - Number(idade)
+    })
+
+    setName("")
+    setIdade(0)
+  }
+
+  return (
+    <div className="container">
+      <h1 className='text'>Descubra a sua idade.</h1>
+      <form onSubmit={handleResult}>
+        <label>
+          <p>Seu nome :</p>
+          <input 
+            type="text" 
+            name="nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Seu ano de nascimento :</p>
+          <input 
+            type="number" 
+            name="nome"
+            value={idade}
+            onChange={(e) => setIdade(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Enviar" className='button'/>
+      </form>
+      {result?.idade && result.name !== '' && (
+        <div className='result'>
+        <p>Olá {result?.name} </p>
+        <p>Você tem {result?.idade} anos</p>
+      </div>
+      )}
+    </div>
+  )
+}
+
+export default App
